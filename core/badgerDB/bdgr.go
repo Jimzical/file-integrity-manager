11b33c1c5e705f0d894aeb09cece2f5b2acc5a29
Jimzical <arishkumar2003@gmail.com>
2024-09-06 22:46:21 +0530

    Use configs directory to manage settings

--- core/badgerDB/bdgr.go
+++ core/badgerDB/bdgr.go
@@ -6,21 +6,25 @@ import (
 	"os"
 
 	badger "github.com/dgraph-io/badger"
+	configs "github.com/Jimzical/file-integrity-manager/configs"
 )
-// Initialize BadgerDB
-//
-// This function initializes BadgerDB and returns a pointer to the database.
-//
-// Returns:
-//   - *badger.DB: A pointer to the BadgerDB database.
-//   - error: An error if the database initialization fails.
-//
-// Example usage:
-//
-//     db, err := InitBadger()
-//     if err != nil {
-//         log.Fatal(err)
-//     }
+
+/*
+Initialize BadgerDB
+
+This function initializes BadgerDB and returns a pointer to the database.
+
+Returns:
+  - *badger.DB: A pointer to the BadgerDB database.
+  - error: An error if the database initialization fails.
+
+Example usage:
+
+    db, err := InitBadger()
+    if err != nil {
+        log.Fatal(err)
+    }
+*/
 func InitBadger() (*badger.DB, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -28,7 +32,7 @@ func InitBadger() (*badger.DB, error) {
 	}
 
 	// Create /badger/filehash directory if it doesn't exist
-	folderName := dbFolderName
+	folderName := configs.BadgerFolderName
 	dbDir := homeDir + "/badger/" + folderName // Fixed path concatenation
 	err = os.MkdirAll(dbDir, os.ModePerm)
 	if err != nil {
@@ -43,22 +47,24 @@ func InitBadger() (*badger.DB, error) {
 	return db, err
 }
 
-// CheckFileHash checks if the hash of a file already exists in the database.
-//
-// This function checks if the hash of a file already exists in the database and adds it if it doesn't.
-//
-// Parameters:
-//   - db: A pointer to the BadgerDB database.
-//   - filepath: The path of the file.
-//   - hash: The hash of the file.
-//
-// Returns:
-//   int: A code indicating the result of the hash check.
-//     NewEntryCode: constant indicating a new entry (value: 0)
-//     HashMatchCode: constant indicating a hash match (value: 1)
-//     HashMismatchCode: constant indicating a hash mismatch (value: 2)
-//     ErrorDuringHashCode: constant indicating an error during hash code (value: 3)
-//   error: An error if the hash check fails.
+/*
+CheckFileHash checks if the hash of a file already exists in the database.
+
+This function checks if the hash of a file already exists in the database and adds it if it doesn't.
+
+Parameters:
+  - db: A pointer to the BadgerDB database.
+  - filepath: The path of the file.
+  - hash: The hash of the file.
+
+Returns:
+  - int: A code indicating the result of the hash check.
+    NewEntryCode: constant indicating a new entry (value: 0)
+    HashMatchCode: constant indicating a hash match (value: 1)
+    HashMismatchCode: constant indicating a hash mismatch (value: 2)
+    ErrorDuringHashCode: constant indicating an error during hash code (value: 3)
+  - error: An error if the hash check fails.
+*/
 func CheckFileHash(db *badger.DB, filepath string, hash string) (int, error) {
 	var storedHash string
 	err := db.View(func(txn *badger.Txn) error {
