520eaafc6c4ee8c2e77a21c7e2b39fbad016ae48
Jimzical <arishkumar2003@gmail.com>
2024-09-08 23:05:35 +0530

    Added Docs

--- ui/print.go
+++ ui/print.go
@@ -4,27 +4,57 @@ import (
 	"fmt"
 )
 
-func Highlight(msg string) {
-	// Apply the style to the print statement
-	fmt.Print(HighlightStyle.Render(msg))
+/*
+Highlight the message with a Green style.
+
+Parameters:
+  - msg: The message to print.
+*/
+func Highlight(msg interface{}) {
+    // Apply the style to the print statement
+    fmt.Print(HighlightStyle.Render(msg.(string)))
 }
 
-func Special(msg string) {
-	// Apply the style to the print statement
-	fmt.Print(SpecialStyle.Render(msg))
+/*
+Style the message with a Green style.
+
+Parameters:
+  - msg: The message to print.
+*/
+func Special(msg interface{}) {
+    // Apply the style to the print statement
+    fmt.Print(SpecialStyle.Render(msg.(string)))
 }
 
-func Important(msg string) {
-	// Apply the style to the print statement
-	fmt.Print(ImportantStyle.Render(msg))
+/*
+Style the message with a White style.
+
+Parameters:
+  - msg: The message to print.
+*/
+func Important(msg interface{}) {
+    // Apply the style to the print statement
+    fmt.Print(ImportantStyle.Render(msg.(string)))
 }
 
-func Incorrect(msg string) {
-	// Apply the style to the print statement
-	fmt.Print(IncorrectStyle.Render(msg))
+/*
+Style the message with a Red style.
+
+Parameters:
+  - msg: The message to print.
+*/
+func Incorrect(msg interface{}) {
+    // Apply the style to the print statement
+    fmt.Print(IncorrectStyle.Render(msg.(string)))
 }
 
-func Info(msg string) {
-	// Apply the style to the print statement
-	fmt.Print(InfoStyle.Render(msg))
+/*
+Style the message with a Yellow style.
+
+Parameters:
+  - msg: The message to print.
+*/
+func Info(msg interface{}) {
+    // Apply the style to the print statement
+    fmt.Print(InfoStyle.Render(msg.(string)))
 }
